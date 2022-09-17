24f813a621e05cd91301786220ccb09ec972a0bc
Petrovskiy Arsenii <petrovskijarsenij11@gmail.com>
2022-09-17 14:22:56 +0300

    my homework

---
+++ main.js
@@ -0,0 +1,35 @@
+// function message() {
+//     let a='я учу javascript'
+//     console.log(a)
+// }
+// message();
+
+
+// function year() {
+//     var age=+prompt ('Сколько вам лет');
+//     if(age>=60){console.log('Вы пенсионер')}
+//     else{console.log('Вы не пенсионер')}
+// }
+// year();
+
+
+// function c() {
+//     for(let i=1; i<=10;i++){
+//          console.log(i)
+//     } 
+   
+// }
+// c()
+
+
+// function ke() {
+//    let  i = 20;
+//    while(i>=5){
+//     console.log(i);
+//     i--
+//    }
+// }
+// ke()
+
+
+
